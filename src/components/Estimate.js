f260114efd78bace708eed88b2142490cdb79673
Meeran Sheikh <meeranxheikh082@gmail.com>
2025-06-13 04:16:37 -0700

    Update Estimate.js

--- src/components/Estimate.js
+++ src/components/Estimate.js
@@ -1,11 +1,9 @@
+
 import React, { useState, useRef } from "react";
 import axios from "axios";
 import Lottie from "react-lottie";
 import { cloneDeep } from "lodash";
-
-import { makeStyles } from "@mui/styles"; // if you still need legacy styling
-import { useTheme } from "@mui/material/styles";
-
+import { makeStyles, useTheme } from "@mui/material/styles";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
@@ -15,8 +13,8 @@ import Dialog from "@mui/material/Dialog";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
 import TextField from "@mui/material/TextField";
+import Hidden from "@mui/material/Hidden";
 import Snackbar from "@mui/material/Snackbar";
-
 import CircularProgress from "@mui/material/CircularProgress";
 
 import check from "../assets/check.svg";
@@ -396,7 +394,6 @@ const websiteQuestions = [
     active: true,
   },
 ];
-
 
 const Estimate = (props) => {
   const classes = useStyles();
@@ -1225,8 +1222,8 @@ const Estimate = (props) => {
                   )}
                 </Button>
               </Grid>
-              {!medium && (<>
-                       <Grid item style={{ marginBottom: "3em" }}>
+              <Hidden mdUp>
+                <Grid item style={{ marginBottom: "3em" }}>
                   <Button
                     variant="text"
                     color="secondary"
@@ -1237,8 +1234,7 @@ const Estimate = (props) => {
                     Cancel
                   </Button>
                 </Grid>
-              </>)}
-
+              </Hidden>
             </Grid>
           </Grid>
         </DialogContent>
@@ -1256,3 +1252,4 @@ const Estimate = (props) => {
 };
 
 export default Estimate;
+
